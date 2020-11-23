52ce456de7fbff0eb4c32cc0b19cf2376556f252
EC2 Default User <ec2-user@ip-172-31-12-102.ec2.internal>
2020-11-23 08:24:49 +0000

    mod code to filter all brand trends by date

--- application.py
+++ application.py
@@ -6,18 +6,45 @@ import boto3
 application = Flask(__name__)
 
 bucket = "brandtrend"
-file_name = "2020-11-22_The North Face.csv"
+file_name = "2020-11-22_Lululemon.csv"
 
 s3 = boto3.client('s3') 
 # 's3' is a key word. create connection to S3 using default config and all buckets within S3
 
 obj = s3.get_object(Bucket= bucket, Key= file_name) 
 
+
+###
+date = '2020-11-23'
+response = s3.list_objects_v2(Bucket= bucket, Prefix = date)
+
+brands_with_date= [item['Key'] for item in response['Contents']]
+
+brands = []
+trends_df = pd.DataFrame(columns = ['date'])
+for brand in brands_with_date:
+    brand_name = brand[11:-4]
+    brands.append(brand_name)
+    
+    obj = s3.get_object(Bucket= bucket, Key= brand) 
+    temp_df = pd.read_csv(obj['Body'])
+    trends_df = pd.merge(trends_df, temp_df, on = 'date', how = 'outer')
+    
+# trends_df.set_index('date', inplace = True)
+# trends_df = trends_df.transpose()
+records = tuple(trends_df.to_records(index = False))
+headings = brands[:]
+headings.insert(0, 'Date')
+headings = tuple(headings)
+print(headings)
+
+###
+
 # get object and file (key) from bucket
 
-initial_df = pd.read_csv(obj['Body']) # 'Body' is a key word
-records = tuple(initial_df.to_records(index=False))
-headings = ('Date','Trend')
+# initial_df = pd.read_csv(obj['Body']) # 'Body' is a key word
+# records = tuple(initial_df.to_records(index=False))
+# headings = ('Date','Trend')
 
 @application.route("/")
 def table():
